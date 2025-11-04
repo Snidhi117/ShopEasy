66d6e6bdabdaed494ad10d8042ac53f5ae3b927e
Snidhi117 <srinidhi@sesame.market>
2025-11-04 23:20:37 +0530

    feat: add shopeasy e-commerce app

--- products.js
+++ products.js
@@ -4,5 +4,5 @@ const products = [
   { id: 3, name: "Bluetooth Speaker", price: 39.99, image: "https://via.placeholder.com/200x150?text=Speaker" },
   { id: 4, name: "Laptop Sleeve", price: 19.99, image: "https://via.placeholder.com/200x150?text=Laptop+Sleeve" },
   { id: 5, name: "Wireless Mouse", price: 15.99, image: "https://via.placeholder.com/200x150?text=Mouse" },
-  { id: 6, name: "Keyboard", price: 25.99, image: "https://via.placeholder.com/200x150?text=Keyboard" }
+  { id: 6, name: "pedegree", price: 200.99, image: "https://via.placeholder.com/200x150?text=pedegree" }
 ];
